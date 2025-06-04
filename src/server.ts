865489f14246f9e54bac9f69270b053f1a9e69c2
Pavel Kratochvíl <pajak235@seznam.cz>
2025-06-04 14:20:24 +0200

    Přidání TypeScriptu

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 const express = require("express");
+// Připojeí api
+const quizApi = require("./api/quizApi.ts");
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const PORT: number = Number(process.env.PORT);
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 app.listen(PORT, () => {
   console.log(`server is running on: Port:${PORT}`);
 });
+
+module.exports = app;
