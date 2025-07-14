010edd25a4a28e317da6bef0397c4c17d155956f
Pavel Kratochvíl <pajak235@seznam.cz>
2025-07-14 09:18:47 +0200

    Přidání CORS

--- src/api/apiRunner.ts
+++ src/api/apiRunner.ts
@@ -2,6 +2,10 @@ const express = require("express");
 const PORT: number = Number(process.env.PORT);
 const app = express();
 
+//CORS
+const cors = require("cors");
+app.use(cors());
+
 app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Server běží na portu:${PORT}`);
